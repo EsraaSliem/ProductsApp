3e2f6072ecd836dc58b058b2c490c1d0fa77a746
Esraa.Abdelrazik <esraa.abdelrazik.external@atos.net>
2022-12-10 21:05:27 +0200

    init project

---
+++ ProductsApp/ProductsApp/ProductsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsAppApp.swift
+//  ProductsApp
+//
+//  Created by Esraa Sliem on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProductsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
